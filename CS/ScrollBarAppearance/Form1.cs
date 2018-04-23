f5720f885e15af5629e24142808e05b93b3c702b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ScrollBarAppearance/Form1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+// ...
+
+namespace ScrollBarAppearance {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            XYDiagram diagram = (XYDiagram)chartControl1.Diagram;
+            diagram.EnableAxisXScrolling = true;
+            diagram.EnableAxisYScrolling = true;
+            diagram.EnableAxisXZooming = true;
+            diagram.EnableAxisYZooming = true;
+            ScrollBarOptions scrollBarOptions = diagram.DefaultPane.ScrollBarOptions;
+            scrollBarOptions.BackColor = Color.White;
+            scrollBarOptions.BarColor = Color.Blue;
+            scrollBarOptions.BorderColor = Color.Navy;
+            scrollBarOptions.BarThickness = 15;
+            scrollBarOptions.XAxisScrollBarAlignment = ScrollBarAlignment.Far;
+            scrollBarOptions.XAxisScrollBarVisible = true;
+        }
+    }
+}
